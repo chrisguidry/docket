70f7c9a714bf0a542f584bee100d33aa4156e9ab
Alex Streed <ajstreed1@gmail.com>
2025-10-24 16:01:23 -0500

    Revert some changes

--- src/docket/__main__.py
+++ src/docket/__main__.py
@@ -1,5 +1,3 @@
-from .cli import (
-    app,
-)  # pragma: no cover covered by tests/cli/test_module.py but that doesn't seem to get picked up
+from .cli import app
 
-app()  # pragma: no cover
+app()
